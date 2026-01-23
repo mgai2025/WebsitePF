cf80c287e0d81f686e295756f0bc034637a94faa
Mohit <mohitgupta.2005@gmail.com>
2026-01-23 19:59:56 +0530

    Add Google Tag Manager scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 export const metadata = {
     title: 'Perfact Food | Agricultural Products from India to UK & Europe',
@@ -81,7 +82,22 @@ export default function RootLayout({ children }) {
                     }}
                 />
             </head>
+            <Script id="google-tag-manager" strategy="afterInteractive">
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WZ5VN6RV');`}
+            </Script>
             <body className="min-h-screen flex flex-col">
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-WZ5VN6RV"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 {children}
                 <GoogleAnalytics gaId="G-7C0YGV18NZ" />
             </body>
